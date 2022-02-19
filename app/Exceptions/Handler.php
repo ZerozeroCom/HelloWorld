9046983d107dd4fec256901a0afeae30893278f5
ZerozeroCom <zerozerocomputer@gmail.com>
2022-02-19 16:43:52 +0800

    錯誤處理修正

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Request;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -34,9 +35,6 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-
-        $this->reportable(function (Throwable $e) {
-            return response()->view('dashboard');
-        });
+        //
     }
 }
